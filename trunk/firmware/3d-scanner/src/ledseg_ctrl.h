94be11ad55ae20419c6555871ebba1845ee51ac5
shikai.chn@gmail.com <shikai.chn@gmail.com@95ce5b2a-e4f2-0052-f926-3f4e88add2a9>
2011-11-30 03:58:29 +0000

    added GPLv2 license info

--- trunk/firmware/3d-scanner/src/ledseg_ctrl.h
+++ trunk/firmware/3d-scanner/src/ledseg_ctrl.h
@@ -9,8 +9,18 @@
  *
  *
  *    LED Seg Ctrl
+ *
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
-
+ 
 #pragma once
 
 #define BIT_SEG1_EN  (0x1L<<0)
