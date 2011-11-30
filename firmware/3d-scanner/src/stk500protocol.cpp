95d3ded7c25527fbbb21b7c4ab8a0122060adde0
shikai.chn <shikai.chn@gmail.com>
2011-11-30 03:58:29 +0000

    added GPLv2 license info

--- firmware/3d-scanner/src/stk500protocol.cpp
+++ firmware/3d-scanner/src/stk500protocol.cpp
@@ -4,6 +4,16 @@
  *
  *  Due to firmware size limitation, the Sunjar RF AP also uses STK500v2 protocol to 
  *  communicate with the PC.
+ *
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 
--- firmware/3d-scanner/src/usb_func.cpp
+++ firmware/3d-scanner/src/usb_func.cpp
@@ -1,4 +1,23 @@
 /*
+ *    RoboPeak Project
+ *    http://www.robopeak.com
+ *
+ *    3D Scanner
+ *    An applet of RoboticPeak Project
+ *
+ *
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/*
  * usb_func.cpp
  *
  *  Created on: Jun 26, 2010
